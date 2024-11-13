85637e2ca692207ff18419fc28360895ac836a60
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-11-13 20:32:10 -0300

    Implements router and test with button

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,19 @@
 import { Button, TextField } from '@mui/material';
 import './App.css'
 import Typography from '@mui/material/Typography';
+import { useNavigate } from "react-router-dom";
+
 function App() {
+  const navigate = useNavigate();
 
   return (
     <div className='container'>
       <Typography variant="h5">Olá, Seja Bem-vindo!</Typography>
       <div className='inputs'>
         <TextField id="outlined-basic" label="Digite seu Nome:" variant="outlined" fullWidth />
-        <Button variant="contained" fullWidth>Entrar</Button>
+        <Button variant="contained" fullWidth onClick={() => {
+          navigate('/dashboard', { replace: true }) // TODO: implements some function to validate if field fill 
+        }}>Entrar</Button>
       </div>
     </div>
   )
