8ac95d2ce7c043a0c04cfd485e8e2e6a28b6928d
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-11-14 01:51:44 -0300

    Implement ThemeProvider and change second color

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,13 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/routes';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/routes";
+import { createTheme, ThemeProvider } from "@mui/material";
 
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: '#EC6724',
+    },
+  },
+});
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>
+);
