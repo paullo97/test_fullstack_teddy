8a1f1bbbba7c0d020d39889f92a15b1b44877b3b
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-11-14 02:50:38 -0300

    WIP - Imeplements some interfaces and inputs

--- front/src/modals/formClient.tsx
+++ front/src/modals/formClient.tsx
@@ -2,20 +2,59 @@ import {
   Button,
   Dialog,
   DialogActions,
+  DialogContent,
   DialogTitle,
   IconButton,
+  Stack,
+  TextField,
   Typography,
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+import { useState } from "react";
+import { useFormik } from "formik";
 
 interface IModalProps {
   open: boolean;
   handleClose: () => void;
+  idClient?: number;
 }
 
-const ModalFormClient: React.FC<IModalProps> = ({ open, handleClose }) => {
+interface IInitialState {
+  name: string;
+  salary: string;
+  enterpriseValue: string;
+}
+
+const INITIAL_STATE: IInitialState = {
+  name: "",
+  salary: "",
+  enterpriseValue: "",
+};
+
+const ModalFormClient: React.FC<IModalProps> = ({ open, handleClose, idClient }) => {
+  const [initialValues] = useState({ ...INITIAL_STATE });
+  
+  const isEditMode = !!Number(idClient);
+  
+  const onSubmit = (data: IInitialState) => {
+    console.log(data);
+  };
+
+  const { values, handleBlur, handleChange } = useFormik({
+    validateOnBlur: true,
+    validateOnChange: false,
+    enableReinitialize: true,
+    initialValues,
+    onSubmit,
+  });
+
   return (
-    <Dialog open={open} onClose={handleClose} sx={{ '& .MuiDialog-paper': { width: '100%', maxHeight: 435 } }} maxWidth='md'>
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      sx={{ "& .MuiDialog-paper": { width: "30%", maxHeight: 435 } }}
+      maxWidth="md"
+    >
       <DialogTitle
         sx={{
           display: "flex",
@@ -24,7 +63,7 @@ const ModalFormClient: React.FC<IModalProps> = ({ open, handleClose }) => {
         }}
       >
         <Typography variant="h6">
-          <strong>Criar Cliente:</strong>
+          <strong>{isEditMode ? 'Editar' : 'Criar'} Cliente:</strong>
         </Typography>
         <IconButton
           onClick={handleClose}
@@ -36,6 +75,39 @@ const ModalFormClient: React.FC<IModalProps> = ({ open, handleClose }) => {
         </IconButton>
       </DialogTitle>
 
+      <DialogContent>
+        <Stack direction="column" sx={{ gap: '10px' }}>
+          <TextField
+            fullWidth
+            label="Digite o Nome:"
+            variant="outlined"
+            value={values.name}
+            name="name"
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <TextField
+            fullWidth
+            label="Digite o Salário:"
+            variant="outlined"
+            value={values.salary}
+            name="salary"
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+
+          <TextField
+            fullWidth
+            label="Digite o Valor da Empresa:"
+            variant="outlined"
+            value={values.enterpriseValue}
+            name="enterpriseValue"
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+        </Stack>
+      </DialogContent>
+
       <DialogActions>
         <Button
           variant="contained"
@@ -43,7 +115,7 @@ const ModalFormClient: React.FC<IModalProps> = ({ open, handleClose }) => {
           fullWidth
           color="secondary"
         >
-          Criar Cliente
+          {isEditMode ? 'Editar' : 'Criar'} Cliente
         </Button>
       </DialogActions>
     </Dialog>
