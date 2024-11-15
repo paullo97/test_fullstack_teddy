4aff267adb168dd171262a49e9345d0fd1ac32a8
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-11-15 09:19:11 -0300

    reference route with component

--- front/src/routes/routes.tsx
+++ front/src/routes/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Dashboard from "../pages/dashboard";
 import DefaultLayout from "../pages/defaultLayout";
+import SelectedClients from "../pages/selectedClients";
 
 export const router = createBrowserRouter([
     {
@@ -16,6 +17,10 @@ export const router = createBrowserRouter([
           path: "",
           element: <Dashboard />,
         },
+        {
+          path: '/dashboard/selecteds',
+          element: <SelectedClients />
+        }
       ],
     },
   ]);
