7e5649ed1e589bc58b8fd003feaa0949a4505232
Paulo Cesar <paulo.cesar@vitaebrasil.com.br>
2024-11-15 10:02:44 -0300

    prepare component to pagination

--- front/src/components/pagination/index.tsx
+++ front/src/components/pagination/index.tsx
@@ -1,15 +1,35 @@
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 
-interface IPagination { 
-    count?: number;
-    spacing?: number;
+interface IPagination {
+  count: number;
+  page: number;
+  spacing?: number;
+  onChange: (event: React.ChangeEvent<unknown>, page: number) => void;
 }
 
-const PaginationComponent: React.FC<IPagination> = ({ count = 10, spacing = 2 }) => {
+const PaginationComponent: React.FC<IPagination> = ({
+  page,
+  count = 10,
+  spacing = 2,
+  onChange,
+}) => {
   return (
-    <Stack spacing={spacing} direction='row' sx={{ justifyContent: 'center', marginTop: '1%' }}>
-      <Pagination count={count} size='large' shape="rounded" color='secondary' hidePrevButton hideNextButton/>
+    <Stack
+      spacing={spacing}
+      direction="row"
+      sx={{ justifyContent: "center", marginTop: "1%" }}
+    >
+      <Pagination
+        count={count}
+        page={page}
+        onChange={onChange}
+        size="large"
+        shape="rounded"
+        color="secondary"
+        hidePrevButton
+        hideNextButton
+      />
     </Stack>
   );
 };
